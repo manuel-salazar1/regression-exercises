545ff8adb223f2ef423f192fff5cb0ccf0f27773
Manuel Salazar <manuel11salazar@gmail.com>
2023-05-11 17:02:51 -0500

    May 11 push

---
+++ evaluate.py
@@ -0,0 +1,92 @@
+# Regression functions
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+def plot_residuals(y, yhat):
+    '''
+    send in: 
+    i.e. y = train.tax_value
+      yhat = train.yhat
+    '''
+    
+    residuals = y - yhat
+    sns.scatterplot(y=residuals, x=y, hue=y, size=y)
+    
+    baseline = y.mean()
+    plt.axhline(baseline, ls=':', color='red')
+    
+    plt.xlabel('Actual')
+    plt.ylabel('Residual')
+    plt.title('Residual Plot')
+    plt.show()
+
+
+
+def regression_errors(y, yhat):
+    '''
+    send in: 
+    i.e.
+    y = train.tax_value
+    yhat = train.yhat
+    return:
+    SSE, ESS, TSS, MSE, RMSE
+    '''
+    MSE = mean_squared_error(y, yhat)
+    SSE = MSE * len(y)
+    RMSE = MSE ** .5
+    
+    ESS = ((yhat - y.mean()) ** 2).sum()
+    TSS = ESS + SSE
+    
+    
+    
+    print(f'Metric  Model_error')
+    print(f'SSE:   ', SSE)
+    print(f'ESS:   ', ESS)
+    print(f'TSS:   ', TSS)
+    print(f'MSE:   ', MSE)
+    print(f'RMSE:  ', RMSE)
+    
+    return SSE, ESS, TSS, MSE, RMSE
+
+def baseline_mean_errors(y):
+    '''
+    send in: 
+    df
+    y = target variable as string
+    You NEED to have a baseline column in df for this function to work
+    return SSE_baseline, MSE_baseline, RMSE_baseline
+    '''
+    
+    baseline = np.repeat(y.mean(), len(y))
+    MSE_baseline = mean_squared_error(y, baseline)
+    SSE_baseline = MSE_baseline * len(y)
+    RMSE_baseline = MSE_baseline ** .5
+    
+    
+    print(f'                 error')
+    print(f'SSE Baseline: ', sse_baseline)
+    print(f'MSE baseline: ', mse_baseline)
+    print(f'RMSE Baseline:', rmse_baseline)
+    
+    return SSE_baseline, MSE_baseline, RMSE_baseline
+
+def better_than_baseline(y, yhat, baseline):
+    '''
+    ex: train.tax_value for y
+    train.yhat for yhat
+    train.baseline for baseline
+    '''
+    sse_model = np.sum((y - yhat) ** 2)
+    sse_baseline = np.sum((y - baseline) ** 2)
+
+    return sse_model < sse_baseline
+
+
+
+
+
+
---
+++ prepare.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.model_selection import train_test_split
+
+
+
+
+# This function will create dummy variable columns
+
+def create_dummy_variables(df, dummy_cols):
+    '''
+    inputs:
+    df , variable with list of strings or single string
+    output:
+    df with dummy columns
+    '''
+    dummy_df = pd.get_dummies(df[dummy_cols], drop_first=True)
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
+
+#--------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
+
+
+# This function will scale data with Min Max scaler
+
+def scale_data(train,
+              validate,
+              test,
+              to_scale):
+    '''
+    create to_scale variable with a list of columns you want to scale
+    returns:
+    train_scaled, validate_scaled, test_scaled
+    '''
+    # make copies for scaling
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
+    
+    # Make the thing
+    scaler = MinMaxScaler()
+    
+    #fit the thing
+    scaler.fit(train[to_scale])
+    
+    #use the thing
+    train_scaled[to_scale] = scaler.transform(train[to_scale])
+    validate_scaled[to_scale] = scaler.transform(validate[to_scale])
+    test_scaled[to_scale] = scaler.transform(test[to_scale])
+    
+    return train_scaled, validate_scaled, test_scaled
+
+
+
+#--------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
+
+
+
+
+# create function to initiate X_y train, validate, test
+
+def Xy_train_val_test(train, validate, test, target_variable):
+    """
+    input train, validate, test, after using split function()
+    input target_variable as string
+    drop_cols formatted as: ['col1', 'col2', 'etc'] for multiple columns
+        This function will drop all 'object' columns. Identify additional 
+        columns you want to drop and insert 1 column as a string or multiple
+        columns in a list of strings.
+    returns:
+    X_train, X_validate, X_test, y_train, y_validate, y_test
+    """
+    
+    baseline_accuracy = train[target_variable].value_counts().max() / train[target_variable].value_counts().sum()
+    print(f'Baseline Accuracy: {baseline_accuracy:.2%}')
+    
+    X_train = train.select_dtypes(exclude=['object']).drop(columns=[target_variable]).drop(columns=drop_cols)
+    X_validate = validate.select_dtypes(exclude=['object']).drop(columns=[target_variable]).drop(columns=drop_cols)
+    X_test = test.select_dtypes(exclude=['object']).drop(columns=[target_variable]).drop(columns=drop_cols)
+    
+    y_train = train[target_variable]
+    y_validate = validate[target_variable]
+    y_test = test[target_variable]
+    
+    return X_train, X_validate, X_test, y_train, y_validate, y_test
+
+
+
+
+
+
+
+
+
